ac291d5a950dbcaf29756d60639425f01c4f332b
HugoTby <hugotabarylapro@gmail.com>
2024-01-16 11:20:06 +0100

    commit

--- default.js
+++ default.js
@@ -1,35 +1,33 @@
-let intervalId;
-const FRAMERATE = 30;
+document.addEventListener('DOMContentLoaded', function () {
+  let intervalId;
+  const FRAMERATE = 2;
 
-window.onload = function () {
-    const canvas = document.getElementById('ambilight');
-    const context = canvas.getContext('2d');
+  const canvas = document.getElementById('ambilight');
+  const context = canvas.getContext('2d');
+  const video = document.getElementById('player');
 
-    const video = document.getElementById('player');
+  function repaintAmbilight() {
+    // Réduire la taille de l'image à 1/3 de la taille d'origine (ajustez selon vos besoins)
+    const scaledWidth = video.videoWidth / 3;
+    const scaledHeight = video.videoHeight / 3;
+  
+    context.drawImage(video, 0, 0, scaledWidth, scaledHeight);
+  }
+  
+  function startAmbilightRepaint() {
+    intervalId = window.setInterval(repaintAmbilight, 1000 / FRAMERATE);
+  }
 
-    function repaintAmbilight() {
-        context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-    }
+  function stopAmbilightRepaint() {
+    clearInterval(intervalId);
+  }
 
-    function startAmbilightRepaint() {
-        intervalId = window.setInterval(repaintAmbilight, 1000 / FRAMERATE);
-    }
+  video.addEventListener('play', startAmbilightRepaint);
+  video.addEventListener('pause', stopAmbilightRepaint);
+  video.addEventListener('ended', stopAmbilightRepaint);
+  video.addEventListener('seeked', repaintAmbilight);
+  video.addEventListener('load', repaintAmbilight);
 
-    function stopAmbilightRepaint() {
-        clearInterval(intervalId);
-    }
-
-    video.addEventListener('play', startAmbilightRepaint);
-
-    video.addEventListener('pause', stopAmbilightRepaint);
-
-    video.addEventListener('ended', stopAmbilightRepaint);
-
-    video.addEventListener('seeked', repaintAmbilight);
-
-    video.addEventListener('load', repaintAmbilight);
-
-    // load first frame
-    repaintAmbilight();
-    video.currentTime = 0;
-};
+  repaintAmbilight(); // Load first frame
+  video.currentTime = 0;
+});
